79aeaf266692ab04d8be0314ec81db97b0b5962f
ffahpatcha1211 <ffahpatcha12@gmail.com>
2026-08-11 01:57:10 +0700

    Update quality_checks_bronze.sql

--- tests/quality_checks_bronze.sql
+++ tests/quality_checks_bronze.sql
@@ -47,7 +47,7 @@ WHERE cst_lastname != TRIM(cst_lastname)
 SELECT DISTINCT cst_gndr
 FROM bronze.crm_cust_info
 
-SELECT DISTINCT cst_material_status
+SELECT DISTINCT cst_marital_status
 FROM bronze.crm_cust_info
 
 /* 
