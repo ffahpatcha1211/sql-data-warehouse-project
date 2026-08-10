3bb28e0eb4c405d20649f8fb46fff87141180199
ffahpatcha1211 <ffahpatcha12@gmail.com>
2026-08-10 22:32:02 +0700

    Update quality_checks_bronze.sql

--- tests/quality_checks_bronze.sql
+++ tests/quality_checks_bronze.sql
@@ -1,3 +1,21 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'bronze' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks data in Bronze Layer.
+===============================================================================
+*/
+
 /* 
 ======================================
 check for clean crm_cust_info table
