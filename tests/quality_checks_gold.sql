cf22ddd2442040c365c47b64f17e95394052fc03
ffahpatcha1211 <ffahpatcha12@gmail.com>
2026-08-11 12:11:08 +0700

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -13,6 +13,87 @@ Usage Notes:
     - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
+/*
+================================================
+check data quality for create gold layer
+================================================
+*/
+
+/*
+check data quality for create customer dimension
+*/
+--after create dimension table check data that is not duplicate
+SELECT cst_id, COUNT(*) 
+FROM
+(
+    SELECT
+        ci.cst_id,
+        ci.cst_key,
+        ci.cst_firstname,
+        ci.cst_lastname,
+        ci.cst_material_status,
+        ci.cst_gndr,
+        ci.cst_create_date,
+        ca.bdate,
+        ca.gen,
+        la.cntry
+    FROM silver.crm_cust_info ci
+    LEFT JOIN silver.erp_cust_az12 ca
+        ON ci.cst_key = ca.cid
+    LEFT JOIN silver.erp_loc_a101 la
+        ON ci.cst_key = la.cid
+) t
+GROUP BY cst_id
+HAVING COUNT(*) > 1;
+
+-- similar data check
+SELECT DISTINCT
+    ci.cst_gndr,
+    ca.gen
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid
+ORDER BY 1, 2;
+
+-- มี config ระหว่าง male,female ก็จะต้องไปดูว่าจะเอาตารางไหนเป็น master แล้วก็ยึดข้อมูลจาก table นั้น เช่นให้ crm เป็น master
+SELECT DISTINCT
+    ci.cst_gndr,
+    ca.gen,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender Info
+        ELSE COALESCE(ca.gen, 'n/a')
+    END AS new_gen
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid
+ORDER BY 1, 2;
+
+/*
+check data quality for create product dimension
+*/
+--check second table doesn't cause duplicate (each table doesn't contain history) each product is one record
+SELECT prd_key, COUNT(*) FROM (
+SELECT
+    pn.prd_id,
+    pn.cat_id,
+    pn.prd_key,
+    pn.prd_nm,
+    pn.prd_cost,
+    pn.prd_line,
+    pn.prd_start_dt,
+    pc.cat,
+    pc.subcat,
+    pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL )t -- Filter out all historical data 
+GROUP BY prd_key
+HAVING COUNT(*) > 1
 
 -- ====================================================================
 -- Checking 'gold.dim_customers'
